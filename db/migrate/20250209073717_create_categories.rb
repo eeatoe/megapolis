62f8fc0348b240bbd48f8df6ab0890750a95a6d5
Sergey Smirnov <eeatoe@gmail.com>
2025-02-20 09:33:59 +0100

    Fixed multiple bugs in database migrations

--- db/migrate/20250209073717_create_categories.rb
+++ db/migrate/20250209073717_create_categories.rb
@@ -1,10 +1,13 @@
 class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
-      t.string :name, null: false, unique: true
-      t.string :slug, null: false, unique: true
+      t.string :name, null: false
+      t.string :slug, null: false
       
       t.references :parent, foreign_key: { to_table: :categories }, null: true
+
+      t.index :name, unique: true
+      t.index :slug, unique: true
 
       t.timestamps
     end
--- db/migrate/20250209124039_create_users.rb
+++ db/migrate/20250209124039_create_users.rb
@@ -1,19 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :email, unique: true, null: false
+      t.string :email, null: false
       t.string :password_digest, null: false
       t.string :name, null: false
-      t.string :role, default: "customer", null: false
+      t.string :role, null: false, default: "customer"
+
+      t.index :email, unique: true
 
       t.timestamps
     end
 
-    add_index :users, :email, unique: true
-
     # Ограничение на возможные роли пользователя
     execute <<-SQL
-      ALERT TABLE users ADD CONSTRAINT role_check CHECK (role IN ('customer', 'admin'));
+      ALTER TABLE users ADD CONSTRAINT role_check CHECK (role IN ('customer', 'admin'));
     SQL
 
   end
